af7780861203243d8cc8c962f92294b7f23ecc01
iammiori <mioriii@naver.com>
2020-11-30 20:58:48 +0900

    [feat] init project

---
+++ talk_using_firebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  talk_using_firebase
+//
+//  Created by miori Lee on 2020/11/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
