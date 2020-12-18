e0142c05080a1596cf0712c75ee10d51db54e7c7
iammiori <mioriii@naver.com>
2020-12-19 00:57:47 +0900

    [feat] 콜렉션뷰 cell 정의

---
+++ autolayout_practice/Cells/mainCollection/mainCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  mainCollectionViewCell.swift
+//  autolayout_practice
+//
+//  Created by miori Lee on 2020/12/18.
+//
+
+import UIKit
+
+class mainCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var placeLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
