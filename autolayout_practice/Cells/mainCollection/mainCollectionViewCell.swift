f7d2fc41e00fd75d5621808f81f5e07d9db37b3f
iammiori <mioriii@naver.com>
2020-12-21 19:26:10 +0900

    [feat] collectionview 둥글게 radius 수정

--- autolayout_practice/Cells/mainCollection/mainCollectionViewCell.swift
+++ autolayout_practice/Cells/mainCollection/mainCollectionViewCell.swift
@@ -10,9 +10,13 @@ import UIKit
 class mainCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet weak var placeLabel: UILabel!
+    @IBOutlet weak var cellView: UIView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        cellView.layer.borderColor = UIColor.blue.cgColor
+        cellView.layer.borderWidth = 0.5
+        cellView.layer.cornerRadius = 10
     }
 
 }
