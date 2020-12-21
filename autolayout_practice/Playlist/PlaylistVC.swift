b265430ac4901c811deeb3ddf650af35292fe3ee
iammiori <mioriii@naver.com>
2020-12-21 19:26:36 +0900

    [feat] 상세 playlist vc 처리

---
+++ autolayout_practice/Playlist/PlaylistVC.swift
@@ -0,0 +1,23 @@
+//
+//  PlaylistVC.swift
+//  autolayout_practice
+//
+//  Created by miori Lee on 2020/12/21.
+//
+
+import Foundation
+import UIKit
+
+class PlaylistVC: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.navigationController?.navigationBar.topItem?.title = "매치리스트"
+        self.navigationController?.navigationBar.tintColor = .black
+
+    
+    }
+}
